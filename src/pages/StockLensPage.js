61820b73280abe829b9bee7204f7b734a6dabacb
ares6898 <ares6898@gmail.com>
2025-05-14 14:35:15 +0900

    Update StockLensPage.js

--- src/pages/StockLensPage.js
+++ src/pages/StockLensPage.js
@@ -13,28 +13,29 @@ export default function StockLensPage() {
 
       <div className="flex flex-wrap justify-center gap-6 px-4 max-w-md mx-auto">
         <button
-          className="rounded-xl bg-white aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-blue-800 text-lg flex items-center justify-center text-wrap p-4"
+          className="rounded-xl bg-blue-100 aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-blue-800 text-lg flex items-center justify-center text-wrap p-4"
           onClick={() => navigate("/stock-lens/refractive")}
         >
+		
           굴절이상<br />교정렌즈
         </button>
 
         <button
-          className="rounded-xl bg-white aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-blue-800 text-lg flex items-center justify-center text-wrap p-4"
+          className="rounded-xl bg-yellow-100 aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-yellow-900 text-lg flex items-center justify-center text-wrap p-4"
           onClick={() => navigate("/stock-lens/kids")}
         >
           소아&청소년
         </button>
 
         <button
-          className="rounded-xl bg-white aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-blue-800 text-lg flex items-center justify-center text-wrap p-4"
+          className="rounded-xl bg-purple-100 aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-purple-900 text-lg flex items-center justify-center text-wrap p-4"
           onClick={() => navigate("/stock-lens/photochromic")}
         >
           변색렌즈 
         </button>
 
         <button
-          className="rounded-xl bg-white aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-blue-800 text-lg flex items-center justify-center text-wrap p-4"
+          className="rounded-xl bg-emerald-100 aspect-square w-40 h-40 shadow-md hover:shadow-lg text-center font-semibold text-emerald-900 text-lg flex items-center justify-center text-wrap p-4"
           onClick={() => navigate("/stock-lens/tinted")}
         >
           착색렌즈
