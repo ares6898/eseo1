8752e9c7c53f7d88d33bd7b10915844e5c206f39
ares6898 <ares6898@gmail.com>
2025-05-15 20:33:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import StockLensPage from "./pages/StockLensPage";
 import RefractiveLensPage from "./pages/RefractiveLensPage";
+import RxLensPage from "./pages/RxLensPage"; // ✅ 추가
+
 import "./index.css";
-
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,8 +15,8 @@ root.render(
       <Route path="/" element={<HomePage />} />
       <Route path="/stock-lens" element={<StockLensPage />} />
       <Route path="/stock-lens/refractive" element={<RefractiveLensPage />} />
+	  <Route path="/rx-lens" element={<RxLensPage />} />  // ✅ 추가
+
     </Routes>
   </Router>
 );
-
-serviceWorkerRegistration.register();
