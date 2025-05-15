6f72260a16e8fe60a3ac7fcd0cafbedd7bd03093
ares6898 <ares6898@gmail.com>
2025-05-15 19:31:57 +0900

    Add files via upload

---
+++ src/serviceWorkerRegistration.js
@@ -0,0 +1,52 @@
+
+const isLocalhost = Boolean(
+  window.location.hostname === 'localhost' ||
+  window.location.hostname === '[::1]' ||
+  window.location.hostname.match(/^127(?:\.(?:[0-9]{1,3})){3}$/)
+);
+
+export function register() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+
+      if (isLocalhost) {
+        checkValidServiceWorker(swUrl);
+      } else {
+        registerValidSW(swUrl);
+      }
+    });
+  }
+}
+
+function registerValidSW(swUrl) {
+  navigator.serviceWorker
+    .register(swUrl)
+    .then(registration => {
+      console.log('ServiceWorker 등록 완료:', registration);
+    })
+    .catch(error => {
+      console.error('ServiceWorker 등록 실패:', error);
+    });
+}
+
+function checkValidServiceWorker(swUrl) {
+  fetch(swUrl)
+    .then(response => {
+      if (
+        response.status === 404 ||
+        response.headers.get('content-type').indexOf('javascript') === -1
+      ) {
+        navigator.serviceWorker.ready.then(registration => {
+          registration.unregister().then(() => {
+            window.location.reload();
+          });
+        });
+      } else {
+        registerValidSW(swUrl);
+      }
+    })
+    .catch(() => {
+      console.log('인터넷 없음. 앱은 오프라인 모드에서 작동할 수 있음.');
+    });
+}
