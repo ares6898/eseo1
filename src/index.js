7fa30f5b3453ce59e87031575a4593fddc84e810
ares6898 <ares6898@gmail.com>
2025-05-23 18:12:38 +0900

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import HomePage from "./pages/HomePage";
 import StockLensPage from "./pages/StockLensPage";
 import RefractiveLensPage from "./pages/RefractiveLensPage";
 import RxLensPage from "./pages/RxLensPage";
-import MultifocalLensPage from "./pages/MultifocalLensPage"; // ✅ 추가
+import MultifocalLensPage from "./pages/MultifocalLensPage";
+import MultifocalLensBasic from "./pages/MultifocalLensBasic";
+import MultifocalLensStandard from "./pages/MultifocalLensStandard";
+import MultifocalLensAdvanced from "./pages/MultifocalLensAdvanced";
+import MultifocalLensSupreme from "./pages/MultifocalLensSupreme";
 
 import "./index.css";
 
@@ -17,7 +21,11 @@ root.render(
       <Route path="/stock-lens" element={<StockLensPage />} />
       <Route path="/stock-lens/refractive" element={<RefractiveLensPage />} />
       <Route path="/rx-lens" element={<RxLensPage />} />
-      <Route path="/multifocal-lens" element={<MultifocalLensPage />} />  {/* ✅ 추가 */}
+      <Route path="/multifocal-lens" element={<MultifocalLensPage />} />
+      <Route path="/multifocal-lens/basic" element={<MultifocalLensBasic />} />
+      <Route path="/multifocal-lens/standard" element={<MultifocalLensStandard />} />
+      <Route path="/multifocal-lens/advanced" element={<MultifocalLensAdvanced />} />
+      <Route path="/multifocal-lens/supreme" element={<MultifocalLensSupreme />} />
     </Routes>
   </Router>
 );
