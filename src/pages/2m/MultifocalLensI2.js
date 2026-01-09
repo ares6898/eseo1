09ce27e99c4598ba6b3f4f63f9ff376b5875b034
ares6898 <ares6898@gmail.com>
2026-01-09 14:29:53 +0900

    Update MultifocalLensI2.js

--- src/pages/2m/MultifocalLensI2.js
+++ src/pages/2m/MultifocalLensI2.js
@@ -101,7 +101,7 @@ export default function MultifocalLensAdvanced() {
   };
 
   return (
-    <div className="relative min-h-screen pb-36 bg-green-200">
+    <div className="relative min-h-screen pb-36 bg-blue-900">
       <div className="max-w-7xl mx-auto px-3 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-7">
         {[...sampleLenses].reverse().map((lens, idx) => {
           const priceRow = selectedRef ? lens.prices.find(p => p.refraction === selectedRef) : null;
@@ -303,4 +303,5 @@ export default function MultifocalLensAdvanced() {
     </div>
   );
 }
+
 
